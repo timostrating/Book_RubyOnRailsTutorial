852f4c53a4b92026778f4636b5fe04e3d89195a7
Timo Strating <timostrating@home.nl>
2017-03-16 12:12:24 +0100

    finished project 7 - Sign up

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email))
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
 end
